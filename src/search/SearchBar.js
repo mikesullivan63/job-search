77bae7ae00f74e23432b6c3266d5f7aca1bea31e
“Mike <mikepsullivan@gmail.com>
2019-09-19 11:04:16 -0500

    Search working pt 1

--- src/search/SearchBar.js
+++ src/search/SearchBar.js
@@ -8,21 +8,52 @@ class SearchBar extends React.Component {
             title: null,
             location: null,
         };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        alert("A name was submitted: '" + this.state.title + "' and '" + this.state.location + "'");
+        event.preventDefault();
+    }
+
+
     render() {
-        <form>
-            <label> 
-                Job Title:
-                <input type="text" value={this.state.title} onChange={this.handleChange} />
-            </label>
-            <br />
-            <label>
-                Location:
-                <input type="text" value={this.state.location} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Search" />
-        </form>
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label> 
+                    Job Title:
+                    <input 
+                        type="text" 
+                        name="title" 
+                        value={this.state.title} 
+                        onChange={this.handleInputChange} 
+                    />
+                </label>
+                <br />
+                <label>
+                    Location:
+                    <input 
+                        type="text" 
+                        name="location" 
+                        value={this.state.location} 
+                        onChange={this.handleInputChange} 
+                    />
+                </label>
+                <input type="submit" value="Search" />
+            </form> 
+        );
     }
 }
 
