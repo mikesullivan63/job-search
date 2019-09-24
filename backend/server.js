33c4a0064a3a326e4f53edcc4a6951331a5aadd7
“Mike <mikepsullivan@gmail.com>
2019-09-24 15:15:17 -0500

    Massive refactor to prep for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const cors = require('cors');
 const PORT = 4000;
 
-app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.options("*", cors());
 
 app.use('/api', require('./routes'));
+app.use(express.static(path.join(__dirname, "/../client/build")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+});
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
+
+
---
+++ client/build/precache-manifest.eb2cd89792b922bb5aec0b6f42cad6cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ce0a80b3bba9fa3b56cadef5b1d2df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "275b823f1265ff63644b",
+    "url": "/static/css/2.f6fac5cc.chunk.css"
+  },
+  {
+    "revision": "33fb2dae2a8926243838",
+    "url": "/static/css/main.5e253372.chunk.css"
+  },
+  {
+    "revision": "275b823f1265ff63644b",
+    "url": "/static/js/2.78fad097.chunk.js"
+  },
+  {
+    "revision": "33fb2dae2a8926243838",
+    "url": "/static/js/main.dc247f3a.chunk.js"
+  },
+  {
+    "revision": "d53a876e1e222e651643",
+    "url": "/static/js/runtime~main.7755a82d.js"
+  }
+]);
