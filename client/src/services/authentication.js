73302b92d31f23ec9914ce0c2cdafda7bcba7ae1
“Mike <mikepsullivan@gmail.com>
2019-10-02 16:26:34 -0500

    WIP really raw additions to React/JWT process

---
+++ client/src/services/authentication.js
@@ -0,0 +1,35 @@
+export const authenticationService = {
+    login,
+    logout
+};
+
+function login(username, password) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+    };
+
+    return fetch(`/user/login`, requestOptions)
+        .then(handleResponse)
+        .then(user => {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            return user;
+        });
+}
+
+function logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+}
+
+export function authHeader() {
+    // return authorization header with jwt token
+    const currentUser = authenticationService.localStorage.getItem('currentUser');
+    if (currentUser && currentUser.token) {
+        return { Authorization: `Bearer ${currentUser.token}` };
+    } else {
+        return {};
+    }
+}
