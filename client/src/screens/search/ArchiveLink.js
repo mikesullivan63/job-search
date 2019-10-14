7d608a090b18f2fdd49e7be8674d9d77f316d3bd
Mike Sullivan <2818418+mikesullivan63@users.noreply.github.com>
2019-10-14 10:32:09 -0500

    Removed logging

--- client/src/screens/search/ArchiveLink.js
+++ client/src/screens/search/ArchiveLink.js
@@ -4,17 +4,15 @@ import { commonMethods } from "./common";
 
 class ArchiveLink extends React.Component {
   archiveLink = (event, jobId) =>
-    commonMethods.addJobToList(event, { jobId }, "/job/archive-job", result => {
-      console.log("Callback in archiveLink");
-      this.props.store.archiveActiveJob(jobId);
-    });
+    commonMethods.addJobToList(event, { jobId }, "/job/archive-job", result =>
+      this.props.store.archiveActiveJob(jobId)
+    );
 
   render() {
     return (
       <button
         className="jobToggleButton"
         onClick={event => {
-          console.log("In archiveLink", this.props.job._id);
           this.archiveLink(event, this.props.job._id);
         }}
       >
