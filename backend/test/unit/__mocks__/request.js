0791d48c65d303bdea6c7c92dc87911b2ad60e3e
Mike Sullivan <2818418+mikesullivan63@users.noreply.github.com>
2019-10-15 10:05:37 -0500

    Working tests

--- backend/test/unit/__mocks__/request.js
+++ backend/test/unit/__mocks__/request.js
@@ -2,10 +2,14 @@ const fs = require("fs");
 const cheerio = require("cheerio");
 
 const determineSource = url => {
-  if (url.indexOf("greenhouse")) {
+  if (url.indexOf("greenhouse") > -1) {
     return "greenhouse";
-  } else if (url.indexOf("lever")) {
+  } else if (url.indexOf("lever") > -1) {
     return "lever";
+  } else if (url.indexOf("hire.withgoogle") > -1) {
+    return "google";
+  } else if (url.indexOf("breezy") > -1) {
+    return "breezy";
   }
 
   throw new Error("Invalid URL: " + url);
--- backend/test/unit/boards.processors.test.js
+++ backend/test/unit/boards.processors.test.js
@@ -1,38 +1,44 @@
-const processGreenhouse = require("../../boards/processors/greenhouse")
-  .processGreenhouse;
-
+const GreenhouseCore = require("../../boards/processors/greenhouse");
+const processGreenhouse = GreenhouseCore.processGreenhouse;
 const processLever = require("../../boards/processors/lever").processLever;
+const processGoogle = require("../../boards/processors/google").processGoogle;
+const processBreezy = require("../../boards/processors/breezy").processBreezy;
+const processWorkable = require("../../boards/processors/workable")
+  .processWorkable;
 
 jest.mock("request-promise-core/configure/request2");
 
+testBoard = (procesor, size) => {
+  return procesor({ name: "test", url: "example" }, "", "", false).then(
+    board => {
+      let jobs = board.jobs.map(job =>
+        [job.title, job.location, job.url].join("").toLowerCase()
+      );
+      jobs.forEach(concatenatedString =>
+        expect(concatenatedString).toEqual(
+          expect.not.stringContaining("undefined")
+        )
+      );
+      expect(jobs.length).toBeGreaterThan(0);
+      expect(jobs.length).toBe(size);
+    }
+  );
+};
+
 describe("Suite of tests to ensure all processors work", () => {
   test("Ensure Greenhouse board works with sample content", () => {
-    return processGreenhouse(
-      { name: "test", url: "example" },
-      "",
-      "",
-      false
-    ).then(board =>
-      board.jobs
-        .map(job => [job.title, job.location, job.url].join("").toLowerCase())
-        .forEach(concatenatedString =>
-          expect(concatenatedString).toEqual(
-            expect.not.stringContaining("undefined")
-          )
-        )
-    );
+    return testBoard(processGreenhouse, 7);
   });
 
   test("Ensure Lever board works with sample content", () => {
-    return processLever({ name: "test", url: "example" }, "", "", false).then(
-      board =>
-        board.jobs
-          .map(job => [job.title, job.location, job.url].join("").toLowerCase())
-          .forEach(concatenatedString =>
-            expect(concatenatedString).toEqual(
-              expect.not.stringContaining("undefined")
-            )
-          )
-    );
+    return testBoard(processLever, 8);
+  });
+
+  test("Ensure Google board works with sample content", () => {
+    return testBoard(processGoogle, 10);
+  });
+
+  test("Ensure Breezy board works with sample content", () => {
+    return testBoard(processBreezy, 4);
   });
 });
