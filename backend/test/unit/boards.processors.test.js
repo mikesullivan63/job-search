9b4acdc6f61496794c0b70970c24cfd798dde2f3
Mike Sullivan <2818418+mikesullivan63@users.noreply.github.com>
2019-10-15 09:05:24 -0500

    Cleanup

--- backend/test/unit/boards.processors.test.js
+++ backend/test/unit/boards.processors.test.js
@@ -1,9 +1,7 @@
 const processGreenhouse = require("../../boards/processors/greenhouse")
   .processGreenhouse;
 
-//jest.resetModules();
 jest.mock("request-promise-core/configure/request2");
-//jest.mock("request");
 
 describe("Suite of tests to ensure all processors work", () => {
   test("Ensure Greenhouse board works with sample content", () => {
