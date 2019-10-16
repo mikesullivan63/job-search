506d0315494ff68d4fd7d03650bb1196903c0ce2
Mike Sullivan <2818418+mikesullivan63@users.noreply.github.com>
2019-10-16 14:46:29 -0500

    Mocks being reset

--- backend/test/jest/list_jobs.test.js
+++ backend/test/jest/list_jobs.test.js
@@ -12,6 +12,22 @@ require("sinon-mongoose");
 
 jest.mock("request-promise-core/configure/request2");
 
+function getAuthToken() {
+  const user = new User();
+  user.firstName = "example";
+  user.lastName = "McEexample";
+  user.email = "example@example.com";
+  const token = jwt.generateJwt(user);
+  console.log("token", token);
+  return token;
+}
+
+afterEach(done => {
+  console.log("Resetting mocks");
+  sinon.restore();
+  done();
+});
+
 afterAll(done => {
   mongoose.disconnect();
   done();
@@ -29,13 +45,23 @@ describe("Suite of tests to ensure all List Job calls work", () => {
   });
 
   it("Ensure list of active jobs is protected by authentication, success", done => {
-    const user = new User();
-    user.firstName = "example";
-    user.lastName = "McEexample";
-    user.email = "example@example.com";
-    const token = jwt.generateJwt(user);
-    console.log("token", token);
+    sinon
+      .mock(UserJobs)
+      .expects("findOne")
+      .chain("exec")
+      .yields(new Error("DB not working"), null);
 
+    return request
+      .get("/job/active-jobs")
+      .set({ Authorization: "Bearer " + getAuthToken() })
+      .expect(500)
+      .end((error, response) => {
+        if (error) return done(error);
+        done();
+      });
+  });
+
+  it("Ensure list of active jobs returns empty list when no object is found", done => {
     sinon
       .mock(UserJobs)
       .expects("findOne")
@@ -44,8 +70,15 @@ describe("Suite of tests to ensure all List Job calls work", () => {
 
     return request
       .get("/job/active-jobs")
-      .set({ Authorization: "Bearer " + token })
+      .set({ Authorization: "Bearer " + getAuthToken() })
       .expect(200)
+      .expect(response => {
+        if (response.body.length !== 0) {
+          throw new Error(
+            "Should have been an empty array, instead was " + response
+          );
+        }
+      })
       .end((error, response) => {
         if (error) return done(error);
         done();
@@ -53,10 +86,6 @@ describe("Suite of tests to ensure all List Job calls work", () => {
   });
 
   /*
-  it("Ensure list of active jobs returns empty list when no object is found", done => {
-
-  });
-
   it("Ensure list of active jobs returns empty list when object is found, but is empty", done => {});
 
   it("Ensure list of active jobs returns list when object is found and has entries", done => {
