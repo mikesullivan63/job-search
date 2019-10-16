59b44636b7ba6c9af6bfd4d760931967a52d0d4f
Mike Sullivan <2818418+mikesullivan63@users.noreply.github.com>
2019-10-15 20:13:11 -0500

    Endpoint testing

--- backend/test/jest/api.test.js
+++ backend/test/jest/api.test.js
@@ -8,7 +8,6 @@ jest.mock("request-promise-core/configure/request2");
 
 describe("Suite of tests to ensure all API's call work", () => {
   it("Ensure list of boards is working", done => {
-    //    try {
     request
       .get("/api/companies")
       .expect(200)
@@ -16,6 +15,35 @@ describe("Suite of tests to ensure all API's call work", () => {
       .expect(response => {
         if (response.body.companies.length === 0) {
           throw new Error("Found wrong set of boards");
+        }
+      })
+      .end((error, response) => {
+        if (error) return done(error);
+        done();
+      });
+  });
+
+  it("Ensure job search works", done => {
+    request
+      .get("/api/Abstract/en/san%20or%20sf")
+      .expect(200)
+      .expect(response => {
+        if (response.body.company !== "Abstract") {
+          throw new Error(
+            "Company name did not come back: " + response.body.company
+          );
+        }
+      })
+      .expect(response => {
+        ///company: board.name, url: board.url, status: "ERROR"
+        if (response.body.status !== "COMPLETED") {
+          throw new Error("Status is incorrect: " + response.body.status);
+        }
+      })
+      .expect(response => {
+        ///company: board.name, url: board.url, status: "ERROR"
+        if (response.body.jobs.length === 0) {
+          throw new Error("No jobs returned");
         }
       })
       .end((error, response) => {
