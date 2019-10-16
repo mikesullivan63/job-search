4840d1de26a10775c1cff1fb9e7524942257908a
Mike Sullivan <2818418+mikesullivan63@users.noreply.github.com>
2019-10-15 21:36:33 -0500

    First pass mongoose Model testing

---
+++ backend/test/jest/mongoose.models.test.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const user = require("../../models/user");
+const User = mongoose.model("User");
+
+testUserModel = (user, property, done) => {
+  user.validate(function(err) {
+    if (property !== null) {
+      expect(err.errors).toHaveProperty(property);
+    } else {
+      expect(err).toBeNull();
+    }
+    done();
+  });
+};
+
+describe("Suite of tests to Mongoose models work", () => {
+  it("Ensure user fails when empty", done => {
+    let user = new User();
+    testUserModel(user, "lastName", done);
+  });
+  it("Ensure user fails when only last name is found", done => {
+    let user = new User();
+    user.lastName = "McEexample";
+    testUserModel(user, "firstName", done);
+  });
+  it("Ensure user fails when only email is not found", done => {
+    let user = new User();
+    user.lastName = "McEexample";
+    user.firstName = "example";
+    testUserModel(user, "email", done);
+  });
+});
