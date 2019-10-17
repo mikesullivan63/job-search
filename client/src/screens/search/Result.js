4f2cf80d9ed04b95bbf1c11c23ae61d72945c48f
Mike Sullivan <2818418+mikesullivan63@users.noreply.github.com>
2019-10-17 11:14:32 -0500

    Basic React Unit tests

--- client/src/screens/search/Result.js
+++ client/src/screens/search/Result.js
@@ -93,11 +93,15 @@ class Result extends React.Component {
           ? "No Matching Jobs"
           : "";
     }
-    let jobsToDisplay = this.props.company.jobs
-      .filter(el => !this.props.store.isIgnored(el))
-      .map(el =>
-        this.props.store.isActive(el) ? this.props.store.getActiveJob(el) : el
-      );
+    let jobsToDisplay = !this.props.company.jobs
+      ? null
+      : this.props.company.jobs
+          .filter(el => !this.props.store.isIgnored(el))
+          .map(el =>
+            this.props.store.isActive(el)
+              ? this.props.store.getActiveJob(el)
+              : el
+          );
     return (
       <div className={displayStyle}>
         <Grid className="display">
