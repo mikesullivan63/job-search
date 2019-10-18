a20cb98fe24e2057efa72f128f099cc5440a95f1
Mike Sullivan <2818418+mikesullivan63@users.noreply.github.com>
2019-10-18 14:20:49 -0500

    Additional logging for Heoku issue

--- backend/boards/processors/core.js
+++ backend/boards/processors/core.js
@@ -24,50 +24,49 @@ module.exports = (board, url, title, location, selectors, debug) => {
         }
       })
       .then($ => {
-        const jobs = selectors
-          .map(selector => {
-            if (debug) {
-              console.log("jobSelector: " + JSON.stringify(selector, null, 2));
-              console.log("$(jobSelector): " + $(selector.jobSelector).length);
-              if ($(selector.jobSelector).length === 0) {
-                console.log(
-                  "jobSelector",
-                  selector.jobSelector + " didn't match anything in Body",
-                  $.html("body")
-                );
+        console.log("selectors: " + JSON.stringify(selectors, null, 2));
+        const jobs = selectors.flatMap(selector => {
+          if (debug) {
+            console.log("jobSelector: " + JSON.stringify(selector, null, 2));
+            console.log("$(jobSelector): " + $(selector.jobSelector).length);
+            if ($(selector.jobSelector).length === 0) {
+              console.log(
+                "jobSelector",
+                selector.jobSelector + " didn't match anything in Body",
+                $.html("body")
+              );
+            }
+          }
+
+          const cheerioResults = $(selector.jobSelector)
+            .filter((i, el) => {
+              if (debug) {
+                console.log("el: " + $(el).html());
+                console.log("title: " + selector.titleExtractor($(el)));
+                console.log("loc: " + selector.locationExtractor($(el), $));
+                console.log("link: " + selector.linkExtractor($(el), $));
               }
-            }
+              return match(title, selector.titleExtractor($(el), $));
+            })
+            .filter((i, el) =>
+              match(location, selector.locationExtractor($(el), $))
+            )
+            .map((i, el) => {
+              return {
+                title: selector.titleExtractor($(el), $),
+                location: selector.locationExtractor($(el), $),
+                url: selector.linkExtractor($(el), $)
+              };
+            });
 
-            const cheerioResults = $(selector.jobSelector)
-              .filter((i, el) => {
-                if (debug) {
-                  console.log("el: " + $(el).html());
-                  console.log("title: " + selector.titleExtractor($(el)));
-                  console.log("loc: " + selector.locationExtractor($(el), $));
-                  console.log("link: " + selector.linkExtractor($(el), $));
-                }
-                return match(title, selector.titleExtractor($(el), $));
-              })
-              .filter((i, el) =>
-                match(location, selector.locationExtractor($(el), $))
-              )
-              .map((i, el) => {
-                return {
-                  title: selector.titleExtractor($(el), $),
-                  location: selector.locationExtractor($(el), $),
-                  url: selector.linkExtractor($(el), $)
-                };
-              });
+          //Needed to convert from Cheerio Object to array of results
+          const results = [];
+          for (let i = 0; i < cheerioResults.length; i++) {
+            results.push(cheerioResults[i]);
+          }
 
-            //Needed to convert from Cheerio Object to array of results
-            const results = [];
-            for (let i = 0; i < cheerioResults.length; i++) {
-              results.push(cheerioResults[i]);
-            }
-
-            return results;
-          })
-          .flat();
+          return results;
+        });
 
         if (debug) {
           console.log("Done parsing: " + jobs.length);
