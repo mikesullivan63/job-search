3a859f432807b53498b8baef3c836ce40b1e8431
Mike Sullivan <2818418+mikesullivan63@users.noreply.github.com>
2019-10-25 16:32:47 -0500

    WIP bad path on watched jobs page

--- client/src/screens/jobs/WatchedJobsPage.js
+++ client/src/screens/jobs/WatchedJobsPage.js
@@ -1,18 +1,74 @@
 import React from "react";
 import { observer } from "mobx-react";
 import { Segment, Grid, Button } from "semantic-ui-react";
-import { setActiveJobs, searchService } from "../../services/search";
+import { searchService } from "../../services/search";
 
 class WatchedJobsPage extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      jobs: []
+      jobs: this.props.store.getActiveJobs()
     };
+  }
+
+  componentDidUpdate() {
+    console.log(
+      "Getting state on componentDidUpdate",
+      JSON.stringify(this.state, null, 2)
+    );
   }
 
   componentDidMount() {
     searchService.setActiveJobs();
+    console.log(
+      "Setting state on componentDidMount, before ",
+      JSON.stringify(this.state, null, 2)
+    );
+
+    this.setState({
+      jobs: this.props.store
+        .getActiveJobs()
+        .slice()
+        .map(job => {
+          const { _id, board, url, title, location } = job;
+          return { _id, board, url, title, location, status: "PENDING" };
+        })
+    });
+
+    console.log(
+      "Setting state on componentDidMount, after ",
+      JSON.stringify(this.state, null, 2)
+    );
+
+    this.state.jobs.forEach(job => {
+      console.log("Fetching:  ", job.board, job.title);
+
+      fetch(job.url)
+        .then(response => {
+          if (response.status == 200) {
+            console.log(
+              "Setting status for ",
+              job.board,
+              job.title,
+              "to ACTIVE"
+            );
+            job.status = "ACTIVE";
+          } else {
+            console.log(
+              "Setting status for ",
+              job.board,
+              job.title,
+              "to INACTIVE"
+            );
+            job.status = "INACTIVE - " + response.statusText;
+          }
+        })
+        .catch(error => {
+          console.log("Setting status for ", job.board, job.title, "to ERROR");
+          job.status = "ERROR - " + error;
+        });
+    });
   }
 
   render() {
@@ -29,7 +85,7 @@ class WatchedJobsPage extends React.Component {
             </Grid>
           </Segment>
           {this.props.store.getActiveJobs().map(job => {
-            console.log("Job:", JSON.stringify(job, null, 2));
+            //console.log("Job:", JSON.stringify(job, null, 2));
 
             return (
               <Segment key={job._id}>
@@ -47,7 +103,37 @@ class WatchedJobsPage extends React.Component {
                       Link to Posting
                     </Button>
                   </Grid.Column>
-                  <Grid.Column>???</Grid.Column>
+                  <Grid.Column>
+                    {fetch(job.url)
+                      .then(response => {
+                        if (response.status == 200) {
+                          console.log(
+                            "Setting status for ",
+                            job.board,
+                            job.title,
+                            "to ACTIVE"
+                          );
+                          return "ACTIVE";
+                        } else {
+                          console.log(
+                            "Setting status for ",
+                            job.board,
+                            job.title,
+                            "to INACTIVE"
+                          );
+                          return "INACTIVE - " + response.statusText;
+                        }
+                      })
+                      .catch(error => {
+                        console.log(
+                          "Setting status for ",
+                          job.board,
+                          job.title,
+                          "to ERROR"
+                        );
+                        return "ERROR - " + error;
+                      })}
+                  </Grid.Column>
                 </Grid>
               </Segment>
             );
