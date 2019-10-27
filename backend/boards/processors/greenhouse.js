f21c36e8e3377a89754564cb5f18ed2b8bbcc722
Mike Sullivan <2818418+mikesullivan63@users.noreply.github.com>
2019-10-27 15:00:46 -0500

    Improved URL logic

--- backend/boards/processors/greenhouse.js
+++ backend/boards/processors/greenhouse.js
@@ -36,12 +36,36 @@ exports.processGreenhouse = (board, title, location, debug) => {
   );
 };
 exports.processGreenhouseEmbed = (board, title, location, debug) => {
-  return processGreenhouseCore(
+  return processBoardPage(
     board,
+    "https://boards.greenhouse.io/embed/job_board?for=" + board.url,
     title,
     location,
-    "https://boards.greenhouse.io/embed/job_board?for=" + board.url,
-    "",
+    [
+      {
+        jobSelector: "div.opening",
+        titleExtractor: (el, $) => el.find("a").text(),
+        locationExtractor: (el, $) => el.find("span.location").text(),
+        linkExtractor: (el, $) => {
+          const fullHref = el.find("a").attr("href");
+          console.log("fullHref", fullHref, typeof fullHref);
+
+          const code = /gh_jid=([\d]+)(&|$|\?)/.exec(fullHref.trim());
+
+          console.log("code", JSON.stringify(code, null, 2));
+
+          if (code && code.length > 0) {
+            return (
+              "https://boards.greenhouse.io/embed/job_app?for=" +
+              board.url +
+              "&token=" +
+              code[1]
+            );
+          }
+          return fullHref;
+        }
+      }
+    ],
     debug
   );
 };
